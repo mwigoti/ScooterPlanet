5eaf8890e487113e2a22b347db82dcd7102fcd9d
mwigoti <henrymwoha02@gmail.com>
2024-03-28 01:37:18 -0500

    change

--- apple/urls.py
+++ apple/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path
+
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('bikes/',views.bikespage,name='bikes'),
     path('book/',views.book, name='book'),
-    url(r'^status/(?P<id>\d{1,3} )/$',views.book_process),
-    url(r'^return_status/(?P<id>\d{1,3})/(?P<id1>\d{1,3})/$',views.return_process),
+   re_path(r'^status/(?P<id>\d{1,3} )/$',views.book_process),
+   re_path(r'^return_status/(?P<id>\d{1,3})/(?P<id1>\d{1,3})/$',views.return_process),
     path('cart/',views.cart)
 ]
--- website/urls.py
+++ website/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include,path
 from . import views
+
 urlpatterns = [
     path('',include('apple.urls')),
     path('admin/', admin.site.urls),
